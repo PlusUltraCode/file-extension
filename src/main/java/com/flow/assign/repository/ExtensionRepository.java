138e55b05914815f8205e60a52c426848c5c9b89
PlusUltraCode <kkd06144@naver.com>
2026-01-31 10:36:07 +0900

    feat(Extension) : Extension repository 삭제

--- src/main/java/com/flow/assign/repository/ExtensionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flow.assign.repository;
-
-import com.flow.assign.domain.Extension;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExtensionRepository extends JpaRepository<Extension, Long> {
-}
