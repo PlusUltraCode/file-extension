455af8001ad2c537f7f204b31e18f04ed9956e4d
PlusUltraCode <kkd06144@naver.com>
2026-01-31 10:36:49 +0900

    feat(Extension) : FixedExtensionPolicyRepsitory 생성

---
+++ src/main/java/com/flow/assign/repository/FixedExtensionPolicyRepository.java
@@ -0,0 +1,7 @@
+package com.flow.assign.repository;
+
+import com.flow.assign.domain.FixedExtensionPolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FixedExtensionPolicyRepository extends JpaRepository<FixedExtensionPolicy, String> {
+}
